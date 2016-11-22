13fbf007884d79dd1963ff91bb692ecd03c8f3e3
GVSRohita <gvsrohita@gmail.com>
2016-11-22 13:30:15 +0530

    Final one for submission

--- views/js/main.js
+++ views/js/main.js
@@ -378,7 +378,7 @@ var pizzaElementGenerator = function (i) {
     pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
     pizzaImageContainer.style.width = "35%";
 
-    pizzaImage.src = "images/pizza.png";
+    pizzaImage.src = "images/pizza-min.png";
     pizzaImage.classList.add("img-responsive");
     pizzaImageContainer.appendChild(pizzaImage);
     pizzaContainer.appendChild(pizzaImageContainer);
@@ -535,7 +535,7 @@ document.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < requiredPizzas; i++) {
         elem = document.createElement('img');
         elem.className = 'mover';
-        elem.src = "images/pizza.png";
+        elem.src = "images/pizza-min.png";
         elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
